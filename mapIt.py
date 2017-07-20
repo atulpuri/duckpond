2af6b030607465afa944925bcb6eb460121439f1
Duck <atul-puri@hotmail.com>
2017-07-21 02:01:14 +0530

    fixing typo

--- mapIt.py
+++ mapIt.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #! python3
 # mapIt.py - Launches a map in the browser using an address from the
 # command line or clipboard.
@@ -11,18 +10,5 @@ else:
     # Get address from clipboard.
     address = pyperclip.paste()
 
-=======
-#! python3
-# mapIt.py - Launches a map in the browser using an address from the
-# command line or clipboard.
 
-import webbrowser, sys, pyperclip
-if len(sys.argv) > 1:
-    # Get address from command line.
-    address = ' '.join(sys.argv[1:])
-else:
-    # Get address from clipboard.
-    address = pyperclip.paste()
-
->>>>>>> 725a41b7b793a36b51fedf936861c83146d09e71
 webbrowser.open('https://www.google.com/maps/place/' + address)
