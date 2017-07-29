c36c6ca12860d84d77d3c7f8eab2f76e4f40b1ed
Duck <atul-puri@hotmail.com>
2017-07-29 14:20:01 +0530

    comment

--- timer.py
+++ timer.py
@@ -1,11 +1,12 @@
 #! python3
 # are you really a duck?
+# the ira just sent me a time bomb in the mail!
 
 q = input('are you a duck? y or n \n')
 if q.lower()=='n':
 	print('sucks to be you')
 	import os
-	os.system('shutdown /s /t 900')
+	os.system('shutdown /s /t 30')
 else:
 	print('well then, welcome to the club')
 input()
